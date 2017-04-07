f60cde0b8ebe357d0b48aa604f502a54f6e705be
Kat Gifford <katgiff2007@gmail.com>
2017-04-07 10:46:09 -0700

    add taking value for input selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
 //back end//
-
+function Pizza (toppings, size){
+  this.toppings = toppings;
+  this.size = size;
+  this.cost = 0;
+}
 
 
 //front end//
 $(document).ready(function(){
 
 //takes client name//
-$("form#user-name-input").submit(function(event) {
+  $("form#user-name-input").click(function(event) {
     event.preventDefault();
-
     var inputtedName = $("input#new-customer-name").val();
     console.log(inputtedName);
-
   });
+
+  //takes user topping selections//
+  $("form#user-toppings").submit(function(event) {
+    event.preventDefault();
+    $("input:checkbox[name=tops-p]:checked").each(function(){
+      var proteinTopping = $(this).val();
+      // $('#client-return-toppings').append(proteinTopping);
+      console.log(proteinTopping);
+    });
+
+    $("input:checkbox[name=tops-o]:checked").each(function(){
+      var otherTopping = $(this).val();
+      console.log(otherTopping);
+    });
+  });
+  
 });
