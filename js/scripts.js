aac8fa43df0a772faa0b991faafa026b585cfb8b
Kat Gifford <katgiff2007@gmail.com>
2017-04-07 11:40:26 -0700

    add first object, add function to push collected values to Pizza object. add radio button for pizza size selection to solve for one vs. multiple values

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-//back end//
+//BACK END//
 function Pizza (toppings, size){
-  this.toppings = toppings;
+  this.toppings = [];
   this.size = size;
-  this.cost = 0;
 }
 
-
-//front end//
+//FRONT END//
 $(document).ready(function(){
+ var newPizza = new Pizza ();
 
 //takes client name//
   $("form#user-name-input").click(function(event) {
@@ -19,23 +18,21 @@ $(document).ready(function(){
   //takes user topping selections//
   $("form#user-toppings").submit(function(event) {
     event.preventDefault();
-    $("input:checkbox[name=tops-p]:checked").each(function(){
-      var proteinTopping = $(this).val();
-      // $('#client-return-toppings').append(proteinTopping);
-      console.log(proteinTopping);
-    });
-    $("input:checkbox[name=tops-o]:checked").each(function(){
-      var otherTopping = $(this).val();
-      console.log(otherTopping);
+    $("input:checkbox[name=tops]:checked").each(function(){
+      var allTopping = $(this).val();
+      newPizza.toppings.push(allTopping);
+      console.log(newPizza.toppings);
+      // $('#client-return-toppings').append(allTopping);
     });
   });
 
   //get pizza size//
   $("form#size-pizza").submit(function(event){
     event.preventDefault();
-    $("input:checkbox[name=size]:checked").each(function(){
+    $("input:radio[name=size]:checked").each(function(){
       var pizzaSize = $(this).val();
-      console.log(pizzaSize);
+      newPizza.size = (pizzaSize);
+      console.log(newPizza.size);
     });
   });
 
